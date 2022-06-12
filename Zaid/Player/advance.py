036f742e0de50478e6e1e45e09997a75cc773d57
muntazer <102398991+muntazer995@users.noreply.github.com>
2022-06-12 17:37:30 +0300

    Update advance.py

--- Zaid/Player/advance.py
+++ Zaid/Player/advance.py
@@ -31,11 +31,11 @@ async def alive(event):
                                   caption=DEADLY,
                                   buttons=[
         [
-        Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/Superior_Bots"),
-        Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/Superior_Support")
+        Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/rr8r9"),
+        Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/xl444")
         ],
         [
-        Button.url("• ʀᴇᴘᴏ •", "https://github.com/ITZ-ZAID/Zaid-Vc-Player")
+        Button.url("• ʀᴇᴘᴏ •", "https://github.com/muntazer995/ing")
         ]
         ]
         )
