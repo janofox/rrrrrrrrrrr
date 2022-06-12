da23bf21c1aae67fd8b9cc2321e05a1da95b0e6b
muntazer <102398991+muntazer995@users.noreply.github.com>
2022-06-12 18:38:30 +0300

    Update userbotjoin.py

--- Zaid/Player/userbotjoin.py
+++ Zaid/Player/userbotjoin.py
@@ -9,7 +9,7 @@ from pyrogram.errors import UserAlreadyParticipant
 
 
 @Client.on_message(
-    command(["userbotjoin", f"userbotjoin@{BOT_USERNAME}"]) & ~filters.private & ~filters.bot
+    command(["انضم", f"userbotjoin@{BOT_USERNAME}"]) & ~filters.private & ~filters.bot
 )
 @authorized_users_only
 @errors
@@ -40,7 +40,7 @@ async def join_group(client, message):
         )
         return
     await message.reply_text(
-        f"✅ **userbot succesfully entered chat**",
+        f"✅ **تم الانضمام**",
     )
 
 
