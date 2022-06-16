bfab25f1345404975cd8b4f46443c66eb3b0882b
muntazer <102398991+muntazer995@users.noreply.github.com>
2022-06-16 20:40:54 +0300

    Update play.py

--- Zaid/Player/play.py
+++ Zaid/Player/play.py
@@ -131,7 +131,7 @@ async def generate_cover(thumbnail, title, userid, ctitle):
 
 
     
-@Client.on_message(command(["تشغيل", f"شغل"]) & other_filters)
+@Client.on_message(command(["تشغيل", f"ش", f"شغل", f"p", f"play"]) & other_filters)
 async def play(c: Client, m: Message):
     await m.delete()
     replied = m.reply_to_message
